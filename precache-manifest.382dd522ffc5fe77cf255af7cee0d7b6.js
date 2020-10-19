7882414443625d6633cb7ec89de3b22b2e1dfeef
bcast201 <bcast201@gmail.com>
2020-10-19 17:41:09 -0500

    Updates

---
+++ precache-manifest.382dd522ffc5fe77cf255af7cee0d7b6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f37f8faf285e7d5b0a74b2d468a5a80",
+    "url": "/breweryFinder/index.html"
+  },
+  {
+    "revision": "b5d5729c40365b64a1f2",
+    "url": "/breweryFinder/static/css/2.413a4a32.chunk.css"
+  },
+  {
+    "revision": "1f10a7c6a930a36c30e6",
+    "url": "/breweryFinder/static/css/main.6c0ce548.chunk.css"
+  },
+  {
+    "revision": "b5d5729c40365b64a1f2",
+    "url": "/breweryFinder/static/js/2.0e14c48a.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/breweryFinder/static/js/2.0e14c48a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f10a7c6a930a36c30e6",
+    "url": "/breweryFinder/static/js/main.255bfa58.chunk.js"
+  },
+  {
+    "revision": "db68592445956aae8a2e",
+    "url": "/breweryFinder/static/js/runtime-main.8321b07e.js"
+  },
+  {
+    "revision": "24280d11cc82e03f2d12f1ddfa50061b",
+    "url": "/breweryFinder/static/media/TheSlugAndLion-dLv7.24280d11.ttf"
+  },
+  {
+    "revision": "75d8147914b17ca016f1c60b70146f12",
+    "url": "/breweryFinder/static/media/background.75d81479.jpg"
+  }
+]);
